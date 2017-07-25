1c5e784b65becb30e2eef3b6f4e0f8a7286e8281
hatamasa <masaya19880517@gmail.com>
2017-07-25 03:57:46 +0000

    add mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app70852176@heroku.com'],
-    :password       => ENV['kcjjqunn6982'],
+    :user_name      => ENV['SENDGRID_PASSWORD'],
+    :password       => ENV['SENDGRID_USERNAME'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
